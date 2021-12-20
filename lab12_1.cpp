90b44607e1b9cef53d51efb3d1493c0018033494
bobubear <jedsa.pd@gmail.com>
2021-12-21 01:44:24 +0700

    Lab12_1 Complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,50 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+char grade;
+string randGrade(){
+    grade = rand()%9;
+    switch (grade){
+    case 0:
+        return "A";
+        break;
+    case 1:
+        return "B+";
+        break;
+    case 2:
+        return "B";
+        break;
+    case 3:
+        return "C+";
+        break;
+    case 4:
+        return "C";
+        break;
+    case 5:
+        return "D+";
+        break;
+    case 6:
+        return "D";
+        break;
+    case 7:
+        return "F";
+        break;
+    case 8:
+        return "W";
+        break;
+    }
+    return 0;
+}
+int main(){
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get " << randGrade() << " in this 261102.";
+    return 0;
+}
